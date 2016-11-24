5ef88d2b909dc5b6150c3db89013d4d4457cbcc9
Nelson Chen <nechen@ap.corp.jwt.com>
2016-11-24 21:01:36 +0800

    1124

--- app/js/yt.js
+++ app/js/yt.js
@@ -15,18 +15,51 @@ app.partial.yt = function(){
 
 	// 3. This function creates an <iframe> (and YouTube player)
 	//    after the API code downloads.
-	// var 
-	var player;
+
+	var playing = null;
+
 	window.onYouTubeIframeAPIReady = function () {
-		player = new YT.Player('player');
-	}
 
-	$('.video-background').click(function(){
-		if(player.getPlayerState() === 2){
-			player.playVideo();
-		}else{
-			player.pauseVideo();
+	
+		$('.video-background .video-container').on('init', function(){
+			$('.video-background .video .player').each(function(i){
+
+				$('iframe', this).attr('id', 'player' + i)
+				var player = new YT.Player('player' + i);
+
+				$(this).on('click', function(){
+					player.play();
+				});
+
+				player.pe = this;
+
+			});
+		}).slick({
+			arrows: false
+		}).on('afterChange', function(){
+			if(playing != null){
+				playing.play();
+			}
+		});
+
+		YT.Player.prototype.play = function(){
+			// alert('');
+			if(this.getPlayerState() !== 1){
+				if(playing != null){
+					playing.pauseVideo();	
+				}
+				this.playVideo();
+				playing = this;
+				$(this.pe).addClass('playing');
+				$(this.pe).removeClass('paused');
+				$(this.pe).siblings().addClass('paused');
+
+			}else{
+				this.pauseVideo();
+				playing = null;
+				$(this.pe).removeClass('playing');
+				$(this.pe).addClass('paused');
+			}
 		}
-	});
-
+	};
 };	
